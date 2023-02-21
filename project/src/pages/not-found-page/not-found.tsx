6d42173de905746de34c9ba9988b3fee0043fcdf
ElenaKiseljova <e.a.kiseljova@gmail.com>
2023-02-21 17:15:24 +0200

    change: Not found psge template

--- project/src/pages/not-found-page/not-found.tsx
+++ project/src/pages/not-found-page/not-found.tsx
@@ -1,31 +1,37 @@
 import {Helmet} from 'react-helmet-async';
+import { Link } from 'react-router-dom';
 
 import Header from '../../components/header/header';
 
 function NotFoundPage(): JSX.Element {
   return (
-    <div className="page">
+    <div className="page page--gray page--login">
       <Helmet>
         <title>Страница не найдена - 6 городов</title>
       </Helmet>
       <Header />
 
-      <main className="page__main page__main--not-found">
-        <section className="property">
-          <div className="property__container container">
-            <div className="property__wrapper">
-              <div className="property__name-wrapper">
-                <h1 className="property__name">
-                  404 - Page not found
-                </h1>
-              </div>
+      <main className="page__main page__main--login">
+        <div className="page__login-container container">
+          <section className="login">
+            <h1 className="login__title">404</h1>
+            <p>Page not found</p>
+            <div className="login__form form">
+              <Link to="/" className="login__submit form__submit button" type="submit">Back to Main</Link>
             </div>
-          </div>
-          <section className="property__map map"></section>
-        </section>
+          </section>
+          <section className="locations locations--login locations--current">
+            <div className="locations__item">
+              <a className="locations__item-link" href="#top">
+                <span>Amsterdam</span>
+              </a>
+            </div>
+          </section>
+        </div>
       </main>
     </div>
   );
 }
 
 export default NotFoundPage;
+
